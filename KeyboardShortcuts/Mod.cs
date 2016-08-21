d293c55841749c8b89892917838cd31824399eef
spaar <spaar.modding@gmail.com>
2016-08-21 17:32:36 +0200

    Release 1.2.0

--- KeyboardShortcuts/Mod.cs
+++ KeyboardShortcuts/Mod.cs
@@ -10,7 +10,7 @@ namespace spaar.Mods.KeyboardShortcuts
     public override string DisplayName { get; } = "Keyboard Shortcuts";
     public override string Author { get; } = "spaar";
 
-    public override Version Version { get; } = new Version(1, 1, 3);
+    public override Version Version { get; } = new Version(1, 2, 0);
     public override string VersionExtra { get; } = "";
     public override string BesiegeVersion { get; } = "v0.32";
 
